799a01170ecb488a879ff0148fbccddcd70ff089
2679923982 <2679923982@qq.com>
2017-10-30 19:13:39 +0800

    sad

---
+++ app/src/main/java/aaaaa/myapplicationaa/MainActivity.java
@@ -0,0 +1,14 @@
+package aaaaa.myapplicationaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //第二天
+    }
+}
